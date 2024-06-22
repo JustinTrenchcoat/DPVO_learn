ad4e6e17ad513e302b846cfa4363b0ce0e062a83
Justin Chang <changjm2@students.cs.ubc.ca>
2024-06-22 23:20:31 +0800

    Maybe started to understand it a bit

--- DPVO/dpvo/altcorr/correlation.py
+++ DPVO/dpvo/altcorr/correlation.py
@@ -1,2 +1,15 @@
 import torch
 import cuda_corr
+#cuda_corr is a package that created through the setup.py under the DPVO folder. Every function that is called through the package is related to the source files that this package is linked to. Please refer to the comments in setup.py to see further.
+
+class CorrLayer(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, fmap1, fmap2, coords, ii, jj, radius, dropout):
+        """ forward correlation """
+        #no idea why the triple quotes are here
+        ctx.save_for_backward(fmap1, fmap2, coords, ii, jj)
+        ctx.radius = radius
+        ctx.dropout = dropout
+        corr, = cuda_corr.forward(fmap1, fmap2, coords, ii, jj, radius)
+
+        return corr
