1f2221802dfcd28c344d956f1a530108b9964e0a
Justin Chang <changjm2@students.cs.ubc.ca>
2024-06-23 19:25:37 +0800

    currently work on correlation.py

--- DPVO/dpvo/altcorr/correlation.py
+++ DPVO/dpvo/altcorr/correlation.py
@@ -1,15 +1,35 @@
 import torch
 import cuda_corr
-#cuda_corr is a package that created through the setup.py under the DPVO folder. Every function that is called through the package is related to the source files that this package is linked to. Please refer to the comments in setup.py to see further.
+# cuda_corr is a package that created through the setup.py under the DPVO folder. 
+# Every function that is called through the package is related to the source files that this package is linked to. 
+# Please refer to the comments in setup.py to see further.
 
 class CorrLayer(torch.autograd.Function):
     @staticmethod
     def forward(ctx, fmap1, fmap2, coords, ii, jj, radius, dropout):
         """ forward correlation """
-        #no idea why the triple quotes are here
+        # no idea why the triple quotes are here
         ctx.save_for_backward(fmap1, fmap2, coords, ii, jj)
         ctx.radius = radius
         ctx.dropout = dropout
         corr, = cuda_corr.forward(fmap1, fmap2, coords, ii, jj, radius)
 
         return corr
+    
+    @staticmethod
+    def backward(ctx,grad):
+        """ backward correlation """
+        fmap1,fmap2,coords,ii,jj=ctx.saved_tensors
+
+        if ctx.dropout <1:
+            perm = torch.rand(len(ii), device="cuda") < ctx.dropout
+            coords = coords[:,perm]
+            grad = grad[:,perm]
+            ii = ii[perm]
+            jj = jj[perm]
+        # the '\' is the continuation character, the line length is restricted so it is broken in two parts
+        # and connected by '\'
+        fmap1_grad, fmap2_grad = \
+            cuda_corr.backward(fmap1,fmap2,coords,ii,jj,grad,ctx.radius)
+        
+        return fmap1_grad, fmap2_grad,None,None,None,None,None
--- DPVO/setup.py
+++ DPVO/setup.py
@@ -5,15 +5,40 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 ROOT = osp.dirname(osp.abspath(__file__))
 
 #setup and distribute python packages
+#this file sets up three packages in total, the name for those packages are:
+#cuda_corr, imported in altcorr folder by correlation.py
+#cuda_ba, imported in fastba folder by ba.py
+#lietorch_backends, imported in lietorch folder by group_ops.py
 setup(
     name = 'dpvo',
     packages = find_packages(),
     ext_modules = [
-        CUDAExtension('cuda_corr', sources=['dpvo/altcorr/correlation.cpp','dpvo/altcorr/correlation_kernel.cu'],
+        #creates three CUDAExtension packages
+        CUDAExtension('cuda_corr', 
+                      #the sources of package cuda_corr comes from the following two files.
+                      sources=['dpvo/altcorr/correlation.cpp','dpvo/altcorr/correlation_kernel.cu'],
                       extra_compile_args={
-                          'cxx':['-03'],
-                          'nvcc':['-03'],
-                      }),
-
-    ]
+                          #O3 is the optimization level, still have no idea what this is.
+                          'cxx':['-O3'],
+                          'nvcc':['-O3'],
+                          }),
+        CUDAExtension('cuda_ba',
+                      sources=['dpvo/fastba/ba.cpp', 'dpvo/fastba/ba_cuda.cu'],
+                      extra_compile_args={
+                          'cxx':['-O3']
+                          }),
+        CUDAExtension('lietorch_backends',
+                      include_dirs=[
+                          #combines the ROOT with th following to get two new paths
+                          osp.join(ROOT, 'dpvo/lietorch/include'),
+                          osp.join(ROOT,'thirdparty/eigen-3.4.0')],
+                      sources=[
+                              'dpvo/lietorch/src/lietorch.cpp',
+                              'dpvo/lietrch/src/lietorch_gpu.cu',
+                              'dpvo/lietorch/src/lietorch_cpu.cpp'],
+                    extra_compile_args={'cxx':['-O3'],'nvcc':['-O3'],}),
+    ],
+    cmdclass={
+        'build_ext':BuildExtension
+        }
 )
